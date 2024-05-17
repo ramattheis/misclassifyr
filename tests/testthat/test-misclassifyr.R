cd428541e6ab9735a2b5af77304169ddba86a281
ramattheis <ramattheis@gmail.com>
2024-05-17 08:48:05 -0400

    Adds the outline of tests with usethis

---
+++ tests/testthat/test-misclassifyr.R
@@ -0,0 +1,10 @@
+test_that("misclassifyr works as expected", {
+  # Define test cases
+  data <- ... # Define your test data
+  model_options <- ... # Define model options
+  estimate_options <- ... # Define estimate options
+
+  result <- misclassifyr(data, model_options, estimate_options)
+
+  expect_true(...) # Add appropriate expectations
+})
